3ea8c111de8b8447204b9c40c214cf6fd4b7f35c
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-11-01 18:46:30 +0200

    add consts, remove logs

---
+++ server/consts.js
@@ -0,0 +1,8 @@
+const constants = {
+    SUMMARIZE_URL: 'https://summarizeflow-b74e5wflra-uc.a.run.app',
+    IMAGE_URL: 'https://imagegenerationflow-b74e5wflra-uc.a.run.app',
+    CHAT_URL: 'https://chatflow-b74e5wflra-uc.a.run.app',
+    TRANSLATION_URL: 'https://translationflow-b74e5wflra-uc.a.run.app',
+};
+
+export default constants;
